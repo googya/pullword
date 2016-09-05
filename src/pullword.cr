4e5b0af4792a9cb3894dc375d3ea4420b0628184
leslie.wen <wen.xs@ikcrm.com>
2016-09-05 18:34:33 +0800

    first commit

---
+++ src/pullword.cr
@@ -0,0 +1,25 @@
+require "./pullword/*"
+require "http/client"
+
+module Pullword
+  HOST = "api.pullword.com"
+
+  def self.pullword(source="", threshold=0, debug=1)
+    client = HTTP::Client.new(HOST)
+    result = client.post("/post.php?source=$#{source}&param1=#{threshold}&param2=#{debug}")
+    split_word(result.body, threshold)
+  end
+
+  def self.split_word(result, threshold)
+    words = result.split()
+    words_list = [] of String
+    words.each do |word|
+      next if word.size == 0
+      final_word, possibility = word.split(":")
+      if possibility.to_f >= threshold
+        words_list << word
+      end
+    end
+    words_list
+  end
+end
---
+++ src/pullword/version.cr
@@ -0,0 +1,3 @@
+module Pullword
+  VERSION = "0.1.0"
+end
